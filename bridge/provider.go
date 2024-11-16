3034a73aae2689ee997391fa9ed38b7dbf1c3cbd
jamesstocktonj1 <james.stocktonj@gmail.com>
2024-11-16 17:02:40 +0000

    Add link create/delete handling

--- bridge/provider.go
+++ bridge/provider.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"net"
+	"strings"
 	helloservice "wit-grpc-test/bridge/bindings/local/wit_grpc_test/hello_service"
 	"wit-grpc-test/proto"
 
@@ -11,11 +14,17 @@ import (
 	"google.golang.org/grpc"
 )
 
+var (
+	errComponentAlreadyLinked = errors.New("error: component already linked")
+	errComponentNotLinked     = errors.New("error: component not linked")
+)
+
 type Bridge struct {
 	proto.UnimplementedHelloServiceServer
-	provider *provider.WasmcloudProvider
-	listen   net.Listener
-	server   *grpc.Server
+	provider        *provider.WasmcloudProvider
+	listen          net.Listener
+	server          *grpc.Server
+	linkedComponent string
 }
 
 func (b *Bridge) Init() (err error) {
@@ -36,14 +45,52 @@ func (b *Bridge) Serve() error {
 	return b.server.Serve(b.listen)
 }
 
+func (b *Bridge) Stop() {
+	b.server.GracefulStop()
+}
+
+func (b *Bridge) healthcheck() string {
+	resp := provider.HealthCheckResponse{
+		Healthy: true,
+		Message: "healthy",
+	}
+	msg, err := json.Marshal(&resp)
+	if err != nil {
+		return "unhealthy"
+	}
+	return string(msg)
+}
+
+func (b *Bridge) createLink(link provider.InterfaceLinkDefinition) error {
+	b.provider.Logger.Info("createLink", "link", link)
+	if len(b.linkedComponent) > 0 {
+		return errComponentAlreadyLinked
+	}
+	b.linkedComponent = link.SourceID
+	return nil
+}
+
+func (b *Bridge) deleteLink(link provider.InterfaceLinkDefinition) error {
+	b.provider.Logger.Info("deleteLink", "link", link)
+	if strings.Compare(b.linkedComponent, link.SourceID) == 0 {
+		b.linkedComponent = ""
+	}
+	return nil
+}
+
 func (b *Bridge) Hello(_ctx context.Context, req *proto.HelloRequest) (*proto.HelloResponse, error) {
 	ctx, span := tracer.Start(_ctx, "Hello")
 	defer span.End()
 
+	if len(b.linkedComponent) == 0 {
+		span.RecordError(errComponentNotLinked)
+		return nil, errComponentNotLinked
+	}
+
 	b.provider.Logger.Info("incoming message", "request", req)
 	resp, err := helloservice.Hello(
 		injectTraceHeader(ctx),
-		b.provider.OutgoingRpcClient("wit_grpc_test-hello"),
+		b.provider.OutgoingRpcClient(b.linkedComponent),
 		mapRequest(req),
 	)
 
